3b3078485b505cde14030275a34663726dfbca1a
hunter <iamhunter32@gmail.com>
2022-05-16 00:40:10 -0400

    filling out tests

--- test/test_rstartree.py
+++ test/test_rstartree.py
@@ -1,33 +1,54 @@
+import numpy as np
+from numpy.typing import NDArray
 from unittest import TestCase
-from rstartree import RSNode
+
+from src.rstartree import RStarTree
+from src.boundingbox import Point
 
 
 class TestRSNode(TestCase):
+    @staticmethod
+    def new():
+        return RStarTree(1, 4)
+
     def test_is_leaf(self):
         self.fail()
 
     def test_choose_subtree(self):
+        def test__check_coverage():
+            self.fail()
+
+        def test__find_growth_overlap_perimeter():
+            self.fail()
+
+        def test__check_node_one():
+            self.fail()
+
+        def test__consider_candidates():
+            self.fail()
+
+        def test__best_candidate():
+            self.fail()
+
+        test__check_coverage()
+        test__find_growth_overlap_perimeter()
+        test__check_node_one()
+        test__consider_candidates()
+        test__best_candidate()
+
         self.fail()
 
-    def test__check_coverage(self):
-        self.fail()
-
-    def test__find_growth_overlap_perim(self):
-        self.fail()
-
-    def test__check_node_one(self):
-        self.fail()
-
-    def test__consider_candidates(self):
-        self.fail()
-
-    def test__best_candidate(self):
-        self.fail()
-
-    def test_update_parent_bounds(self):
+    def test_query(self):
         self.fail()
 
     def test_insert(self):
+        tree = self.new()
+        tree.insert(Point(np.array([1, 3])))
+        tree.insert(Point(np.array([2, 2])))
+        tree.insert(Point(np.array([5, 2])))
+        tree.insert(Point(np.array([4, 3])))
+
+        tree.insert(Point(np.array([2, 3])))
         self.fail()
 
     def test_choose_split(self):
