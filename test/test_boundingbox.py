568198c58cfbc9c50c81324bfd78c3e6ac8ddb64
hunter <iamhunter32@gmail.com>
2022-05-15 20:58:29 -0400

    creating test skeleton

---
+++ test/test_boundingbox.py
@@ -0,0 +1,57 @@
+from unittest import TestCase
+
+
+class TestBoundingBox(TestCase):
+    def test_margin_of(self):
+        self.fail()
+
+    def test_volume_of(self):
+        self.fail()
+
+    def test_overlap_sc(self):
+        self.fail()
+
+    def test_create(self):
+        self.fail()
+
+    def test_is_same(self):
+        self.fail()
+
+    def test_asymmetry(self):
+        self.fail()
+
+    def test_center_along(self):
+        self.fail()
+
+    def test_width(self):
+        self.fail()
+
+    def test_center(self):
+        self.fail()
+
+    def test_volume(self):
+        self.fail()
+
+    def test_volume_diff(self):
+        self.fail()
+
+    def test_margin(self):
+        self.fail()
+
+    def test_margin_diff(self):
+        self.fail()
+
+    def test_overlap(self):
+        self.fail()
+
+    def test_overlap_margin(self):
+        self.fail()
+
+    def test_overlap_volume(self):
+        self.fail()
+
+    def test_min_bb_with(self):
+        self.fail()
+
+    def test_encloses(self):
+        self.fail()
---
+++ test/test_rstartree.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+from rstartree import RSNode
+
+
+class TestRSNode(TestCase):
+    def test_is_leaf(self):
+        self.fail()
+
+    def test_choose_subtree(self):
+        self.fail()
+
+    def test__check_coverage(self):
+        self.fail()
+
+    def test__find_growth_overlap_perim(self):
+        self.fail()
+
+    def test__check_node_one(self):
+        self.fail()
+
+    def test__consider_candidates(self):
+        self.fail()
+
+    def test__best_candidate(self):
+        self.fail()
+
+    def test_update_parent_bounds(self):
+        self.fail()
+
+    def test_insert(self):
+        self.fail()
+
+    def test_choose_split(self):
+        self.fail()
+
+    def test_split(self):
+        self.fail()
+
+    def test__determine_dim(self):
+        self.fail()
+
+    def test__minimize_on(self):
+        self.fail()
+
+    def test__compute_wf(self):
+        self.fail()
+
+
+class TestRStarTree(TestCase):
+    def test_insert(self):
+        self.fail()
+
+    def test_overflow_treatment(self):
+        self.fail()
